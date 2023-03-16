77215f797d03207768214cbfa4a0a22923b214dd
Firsova Liubov <firsova.lyub@gmail.com>
2023-03-16 14:26:28 +0200

    Update TweetList.jsx

--- src/components/TweetList/TweetList.jsx
+++ src/components/TweetList/TweetList.jsx
@@ -1,11 +1,12 @@
 import users from 'components/db/users.json';
 import { Tweet } from 'components/Tweet/Tweet';
+import { TweetsList, TweetItem } from './TweetList.styled';
 
 export const TweetList = () => {
   return (
-    <ul>
+    <TweetsList>
       {users.map(({ id, user, tweets, followers, avatar }) => (
-        <li key={id}>
+        <TweetItem key={id}>
           <Tweet
             id={id}
             user={user}
@@ -13,8 +14,8 @@ export const TweetList = () => {
             followers={followers}
             avatar={avatar}
           />
-        </li>
+        </TweetItem>
       ))}
-    </ul>
+    </TweetsList>
   );
 };
