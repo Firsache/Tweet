0b82c0563df7f63bfda97c5fffb8ecaa53e21443
Firsova Liubov <firsova.lyub@gmail.com>
2023-03-16 14:40:01 +0200

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,11 +1,12 @@
 import { GlobalStyles } from 'styles/GlobalStyles/globalStyles.styled';
 import { TweetList } from './TweetList/TweetList';
+import { Wrapper } from './TweetList/TweetList.styled';
 
 export const App = () => {
   return (
-    <>
+    <Wrapper>
       <TweetList />
       <GlobalStyles />
-    </>
+    </Wrapper>
   );
 };
