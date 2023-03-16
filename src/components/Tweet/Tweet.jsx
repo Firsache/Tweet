3bca7d24f5d88b9b704a3d94b6df2c0378bddc51
Firsova Liubov <firsova.lyub@gmail.com>
2023-03-16 15:41:50 +0200

    Update Tweet.jsx

--- src/components/Tweet/Tweet.jsx
+++ src/components/Tweet/Tweet.jsx
@@ -1,7 +1,9 @@
-import { parseDataFromLS, saveToLS } from 'helpers/localStorage';
 import { useEffect, useRef, useState } from 'react';
+import topImg from 'img/top.png';
+import logo from 'img/Logo.png';
 import {
   Container,
+  LogoImg,
   AvatarImg,
   Button,
   EllipceImg,
@@ -10,6 +12,9 @@ import {
   Bottom,
   TopImg,
 } from './Tweet.styled';
+
+import { addComa } from 'helpers/addComaToNum';
+import { parseDataFromLS, saveToLS } from 'helpers/localStorage';
 
 const STORAGE_FOLLOWERS_KEY = 'followersAmount';
 const STORAGE_IS_FOLLOWING_KEY = 'isFollowingAmount';
@@ -55,9 +60,11 @@ export const Tweet = ({ id, user, tweets, followers, avatar }) => {
       setIsFollowing(true);
     }
   };
+  const followersWithComa = addComa(currentFollowers);
   return (
     <Container>
-      <TopImg />
+      <LogoImg src={logo} alt="logo image" />
+      <TopImg src={topImg} alt="background image" />
       <Divider />
       <EllipceImg>
         <AvatarImg src={avatar} alt={user} />
@@ -65,7 +72,7 @@ export const Tweet = ({ id, user, tweets, followers, avatar }) => {
       <Bottom>
         <TweetInfo>
           <p>{tweets} tweets</p>
-          <p>{currentFollowers} followers</p>
+          <p>{followersWithComa} followers</p>
         </TweetInfo>
 
         <Button onClick={handleFollowBtnClick} isFollowing={isFollowing}>
